28ac4ab22ef09f16fed5397fd0fa3d81f15d16e3
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-08-08 23:28:23 +0900

    変換部分を動くように

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ import os
 import requests
 from urllib.parse import urljoin
 import sqlite3
-server_id = 665189315877535753
-
-server_id = int(server_id)
-
 client = discord.Client()
+conn = sqlite3.connect('grass.db')
+c = conn.cursor()
 
 async def send(channel,*args, **kwargs): return await channel.send(*args, **kwargs)
  
@@ -23,19 +21,36 @@ async def on_message(message):
 
     def check(command):
         return command.author == message.author
-    c = await client.wait_for("message", check=check)
-    grass = c.content
+    cc = await client.wait_for("message", check=check)
+    grass = cc.content
     baseurl = "https://github.com"
     url = urljoin(baseurl, grass)
-    grass_image = subprocess.check_output(['curl', url, '|', "awk", "'/<svg.+class="js-calendar-graph-svg"/,/svg>/'", "|", 'sed', '-e', 's/<svg/<svg xmlns="http:\/\/www.w3.org\/2000\/svg"/'])
+    command = '/usr/bin/curl {url} | awk \'/<svg.+class=\"js-calendar-graph-svg\"/,/svg>/\' | sed -e \'s/<svg/<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\"/\' > test.svg'
+    command = command.format(url=url)
+    print(command)
+    grass_image = os.system(command)
     grass_image_name = grass + ".svg"
+    grass_image = str(grass_image)
     f = open(grass_image_name, "w")
     f.write(grass_image)
     f.close()
-    grass_convert_fname = grass + ".png"
-    subprocess.check_output(["convert", grass_image_name, grass_convert_fname])
+    grass_convert_fname = "--output=" + grass + ".png"
+    subprocess.run(["rsvg-convert", "--format=png",  grass_image_name, grass_convert_fname])
+    uname = message.author.id
+    sql = 'insert into grass (username, filename) values (?,?)'
+    namelist = (uname, grass_convert_fname)
+    c.execute(sql, namelist)
+    conn.commit()
     if message.content == '草':
+        user_name = message.author.id
+        select_sql = 'select * from grass where username='
+        username = ('{user_name}')
+        select_sql = select_sql + username
+        c.execute(select_sql)
+        result=c.fetchone()
+        
+        
 
 
 if __name__ == "__main__":
-    client.run(os.environ['MARK1_TOKEN'])
+    client.run()
