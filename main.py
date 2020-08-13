73b661e5cfa8d4161d1f4afb9a18ad964d4aab3b
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-08-13 21:21:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,40 +24,35 @@ async def on_message(message):
         cc = await client.wait_for("message", check=check)
 
         grass = cc.content
+        grass = str(grass)
+        grass = grass.lower()
+        grass = str(grass)
         baseurl = "https://github.com"
+        baseurl = str(baseurl)
         url = urljoin(baseurl, grass)
-        grass_image_name = grass + ".svg"
-        command = '/usr/bin/curl {url} | awk \'/<svg.+class=\"js-calendar-graph-svg\"/,/svg>/\' | sed -e \'s/<svg/<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\"/\''
-        command = command + ">" + "./images/" + grass_image_name
-        command = command.format(url=url)
-        print(command)
-        grass_image = os.system(command)
-        grass_image_name = "./images/" + grass_image_name
-        grass_convert_fname = "--output=" + "./images/" +grass + ".png"
-        subprocess.run(["rsvg-convert", "--format=png",  grass_image_name, grass_convert_fname])
-        uname = message.author.id
-        grass_convert_fname = grass + ".png"
-        #os.remoremove(grass_convert_fname)
-        sql = 'insert into grass (username, filename) values (?,?)'
-        namelist = (uname, grass_convert_fname)
-        c.execute(sql, namelist)
-        conn.commit()
-        # if message.content == '草':
-        #     user_name = message.author.id
-        #     select_sql = 'select * from grass where username='
-        #      username = ('{user_name}').format(user_name=user_name)
-        #     select_sql = select_sql + username
-        #     print(select_sql)
-        #     c.execute(select_sql)
-        #     result=c.fetchone()
-        #     # print(result)
-    #     # DBから出てきたデータは16桁のIDが最初にあるので17文字目まで切り捨てることができる
-    #     # m = result.decode()
-    #     # print(result)
-    #     img_name = result[1]
-    #     await message.channel.send(file=discord.File(img_name))
-    #     print(result[1])
-
+        req = requests.get(url)
+        print(url)
+        print(req)
+        if req.status_code == requests.codes.ok:
+            grass_image_name = grass + ".svg"
+            command = '/usr/bin/curl {url} | awk \'/<svg.+class=\"js-calendar-graph-svg\"/,/svg>/\' | sed -e \'s/<svg/<svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\"/\''
+            command = command + ">" + "./images/" + grass_image_name
+            command = command.format(url=url)
+            print(command)
+            grass_image = os.system(command)
+            grass_image_name = "./images/" + grass_image_name
+            grass_convert_fname = "--output=" + "./images/" +grass + ".png"
+            subprocess.run(["rsvg-convert", "--format=png",  grass_image_name, grass_convert_fname])
+            uname = message.author.id
+            grass_convert_fname = grass + ".png"
+            #os.remoremove(grass_convert_fname)
+            sql = 'insert into grass (username, filename) values (?,?)'
+            namelist = (uname, grass_convert_fname)
+            c.execute(sql, namelist)
+            conn.commit()
+        else:
+            await message.channel.send("存在していないuserです\n最初からやり直してください")
+            return
 
 if __name__ == "__main__":
     client.run(os.environ['LEAF_TOKEN'])
