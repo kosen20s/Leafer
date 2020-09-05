0bd289b9fccac16a797c021874680fce1efe8c9e
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-09-05 16:38:39 +0900

    不要な変数の削除

--- image.py
+++ image.py
@@ -3,8 +3,6 @@ import os
 import sqlite3
 
 client = discord.Client()
-conn = sqlite3.connect('grass.db')
-c = conn.cursor()
 
 async def send(channel,*args, **kwargs): return await channel.send(*args, **kwargs)
 
@@ -13,32 +11,12 @@ async def on_message(message):
     if message.author.bot:
         return
     if message.content == "草":
-        user_name = message.author.id
-        select_sql = 'select * from grass where username='
-        username = ('{user_name}').format(user_name=user_name)
-        select_sql = select_sql + username
-        print(select_sql)
-        c.execute(select_sql)
-        result = c.fetchone()
-        img_name = result[1]
-        img_name = "./images/" + img_name
-        img_name_n = img_name.replace(".png","")
-        img_name_n = img_name_n.replace("./images/","")
-        await message.channel.send(img_name_n)
+        user_name = str(message.author.id)
+        img_name = "./images/" + user_name + ".png"
         await message.channel.send(file=discord.File(img_name))
     if message.content == "grass":
-        user_name = message.author.id
-        select_sql = 'select * from grass where username='
-        username = ('{user_name}').format(user_name=user_name)
-        select_sql = select_sql + username
-        print(select_sql)
-        c.execute(select_sql)
-        result = c.fetchone()
-        img_name = result[1]
-        img_name = "./images/" + img_name
-        img_name_n = img_name.replace(".png","")
-        img_name_n = img_name_n.replace("./images/","")
-        await message.channel.send(img_name_n)
+        user_name = str(message.author.id)
+        img_name = "./images/" + user_name + ".png"
         await message.channel.send(file=discord.File(img_name))
         
 if __name__ == "__main__":
